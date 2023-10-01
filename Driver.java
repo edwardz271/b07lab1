a13b05dcdb5b9e1d4d057f87aaec6e80de85b02e
Edward Zhang <edzhang2004@gmail.com>
2023-10-01 11:27:20 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,152 @@
+package b07lab2fix;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Driver {
-public static void main(String [] args) {
-Polynomial p = new Polynomial();
-System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
-Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
-if(s.hasRoot(1))
-System.out.println("1 is a root of s");
-else
-System.out.println("1 is not a root of s");
-}
+	public static void main(String[] args) throws IOException
+	{
+		// Testing empty case
+		Polynomial p = new Polynomial();
+		System.out.println(p.evaluate(3));
+		// Testing add
+		System.out.println("Testing add");
+//		double[] c1 = {-2, 6, 5};
+//		int[] e1 = {1, 0, 3};
+		double[] c1 = {6, -2, 5};
+		int[] e1 = {0, 1, 3};
+		Polynomial p1 = new Polynomial(c1, e1);
+		double[] c2 = {2, -1, 3, 2};
+		int[] e2 = {0, 1, 2, 3};
+		Polynomial p2 = new Polynomial(c2, e2);
+		Polynomial s = p1.add(p2);
+		System.out.println("First Polynomial Add");
+		for (int i = 0; i < s.Poly_Cf.length; i++) {
+			System.out.println("Coeff = " + s.Poly_Cf[i]);
+			System.out.println("Exp = " + s.Poly_Exp[i]);
+		}
+
+//		double[] bc1 = {5, 9, 22, 10};
+//		int[]  be1 = {3, 7, 23, 4};
+//		Polynomial bp1 = new Polynomial(bc1, be1);
+//		double[] bc2 = {-22, 3, 11, 19, 17};
+//		int[] be2 =     {23, 1, 0, 7, 13};
+		double[] bc1 = {5, 9, 10, 22};
+		int[]  be1 = {3, 7, 4, 23};
+		Polynomial bp1 = new Polynomial(bc1, be1);
+		double[] bc2 = {11, 3, 19, 17, -22};
+		int[] be2 =     {0, 1, 7, 13, 23};
+		Polynomial bp2 = new Polynomial(bc2, be2);
+		Polynomial bct = bp2.add(bp1);
+		System.out.println("Second Polynomial Add");
+		for (int i = 0; i < bct.Poly_Cf.length; i++) {
+			System.out.println("Coeff = " + bct.Poly_Cf[i]);
+			System.out.println("Exp = " + bct.Poly_Exp[i]);
+		}
+
+		// Testing evaluate
+		System.out.println("Testing evaluate");
+		System.out.println("s(1) = " + s.evaluate(1));
+		System.out.println("s(2) = " + s.evaluate(2));
+		System.out.println("bct(0) = " + bct.evaluate(0));
+		System.out.println("bct(1) = " + bct.evaluate(1));
+		System.out.println("bct(2) = " + bct.evaluate(2));
+		// Testing multiply
+		System.out.println("Testing Multiply");
+		//mult 1
+		double[] c3 = {-1.0, 1.0};
+		int[] e3 = {0, 1};
+		Polynomial p3 = new Polynomial(c3, e3);
+		double[] c4 = {-2.0, 1.0};
+		int[] e4 = {0, 1};
+		Polynomial p4 = new Polynomial(c4, e4);
+		Polynomial s2 = p3.multiply(p4);
+		System.out.println("First Multiply");
+		for (int i = 0; i < s2.Poly_Cf.length; i++) {
+			System.out.println("Coeff = " + s2.Poly_Cf[i]);
+			System.out.println("Exp = " + s2.Poly_Exp[i]);
+		}
+		
+		//mult2
+//		double[] c5 = {5, 7, 11};
+//		int[] e5 = {1, 0, 2};
+		double[] c5 = {7, 5, 11};
+		int[] e5 = {0, 1, 2};
+		Polynomial p5 = new Polynomial(c5, e5);
+		double[] c6 = {2, 8, 7, 6};
+		int[] e6 = {0, 1, 2, 3};
+		Polynomial p6 = new Polynomial(c6, e6);
+		Polynomial s3 = p6.multiply(p5);
+		System.out.println("Second multiply");
+		for (int i = 0; i < s3.Poly_Cf.length; i++) {
+			System.out.println("Coeff = " + s3.Poly_Cf[i]);
+			System.out.println("Exp = " + s3.Poly_Exp[i]);
+		}
+		
+		//mult3
+		double[] c8 = {6, -2, 4,1};
+		int[] e8 = {0, 1, 2,3};
+		Polynomial p8 = new Polynomial(c8, e8);
+		double[] c9 = {7, 2};
+		int[] e9 = {0, 1};
+		Polynomial p9 = new Polynomial(c9, e9);
+		Polynomial s10 = p8.multiply(p9);
+		System.out.println("Third multiply");
+		for (int i = 0; i < s10.Poly_Cf.length; i++) {
+			System.out.println("Coeff = " + s10.Poly_Cf[i]);
+			System.out.println("Exp = " + s10.Poly_Exp[i]);
+		}
+		
+		//end mult tests
+		System.out.println("Testing hasRoot");
+		if (s2.hasRoot(2)) {
+			System.out.println("2 is a root of s2");
+		}
+		else {
+			System.out.println("2 is not a root of s2");
+		}
+		if (s2.hasRoot(1)) {
+			System.out.println("1 is a root of s2");
+		}
+		else {
+			System.out.println("1 is not a root of s2");
+		}
+		if (s2.hasRoot(0)) {
+			System.out.println("0 is a root of s2");
+		}
+		else {
+			System.out.println("0 is not a root of s2");
+		}
+		if (s3.hasRoot(-2)) {
+			System.out.println("-2 is a root of s3");
+		}
+		else {
+			System.out.println("-2 is not a root of s3");
+		}
+		if (s3.hasRoot(4)) {
+			System.out.println("4 is a root of s3");
+		}
+		else {
+			System.out.println("4 is not a root of s3");
+		}
+		
+		
+		System.out.println("Testing File reading");
+		File f = new File("C:\\Users\\edzha\\eclipse-workspace\\b07lab2fix\\Test_File_reading");
+		Polynomial p7 = new Polynomial(f);
+		for (int i = 0; i < p7.Poly_Cf.length; i++) {
+			System.out.println("Coeff = " + p7.Poly_Cf[i]);
+			System.out.println("Exp = " + p7.Poly_Exp[i]);
+		}
+		System.out.println("Writing to File");
+		s3.saveToFile("output1.txt");
+		double []lab2Poly_Cf = {5,-3,7};
+		int []lab2Poly_Exp =   {0,2,8};
+		Polynomial lab2 = new Polynomial(lab2Poly_Cf, lab2Poly_Exp);
+		lab2.saveToFile("output2.txt");
+		double []lab2Poly_Cf2 = {6,-2,5};
+		int []lab2Poly_Exp2 =   {0,1,3};
+		Polynomial lab2_2 = new Polynomial(lab2Poly_Cf2, lab2Poly_Exp2);
+		lab2_2.saveToFile("output3.txt");
+	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,9 +1,19 @@
-public class Polynomial {
-    // Instance variables
-    private double[] Poly_Cf;
-    private int[] Poly_Exp;
+package b07lab2fix;
 
-    // Constructor with no args
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Polynomial {
+
+	// Instance variables
+    public double[] Poly_Cf;
+    public int[] Poly_Exp;
+
+    // Constructor with no arguments
     public Polynomial() {
         this.Poly_Cf = new double[]{0};
         this.Poly_Exp =new int[]{0};
@@ -15,121 +25,133 @@ public class Polynomial {
         this.Poly_Exp = exponents;
     }
 
-    public Polynomial(File poly_file){
-            BufferedReader reader = new BufferedReader(Filereader(poly_file));
+    public Polynomial(File poly_file) throws IOException{
+            BufferedReader reader;
+			reader = new BufferedReader(new FileReader(poly_file));
             String txt_poly = reader.readLine();
+            txt_poly = txt_poly.replace("+", " +");
+            txt_poly = txt_poly.replace("-", " -");
+            reader.close(); //or else string not saved
+            
+            //creating array equal to total num of terms in string
+            //length fact provable w induction
             int total_op = 0; 
-            for(int i = 0; i<txt_poly.length; i++){
-                if(txt_poly[i] == '+' || txt_poly[i] == '-'){
+            for(int i = 0; i<txt_poly.length(); i++){
+                if(txt_poly.charAt(i) == '+' || txt_poly.charAt(i) == '-'){
                     total_op++;  
                 }
             }
-            int total_terms = total_op+1;
-
+            int total_terms = total_op+1; 
             double [] coefficient = new double [total_terms];
             int [] exponent = new int [total_terms];
-
-            int index = 0; 
-            String [] no_plus = txt_poly.split("+");
-            for(String ss: no_plus){
-                String [] no_minus = ss.split("-");
-                for(int i = 0; i < no_minus.length;i++){
-                    if(i == 0){
-                        int x_count = 0;
-                        for(int j=0; j<no_minus[i].length; j++){
-                            if(no_minus[i] == 'x'){
-                                x_count++;
-                            }
-                        }
-                        if (x_count == 0){
-                                coefficient[index] = no_minus[i];
-                                expoenent[index] = 0;
-                        } 
-                        else{
-                               String [] cf_then_exp = no_minus[i].split("x");
-                               coefficient[index] = cf_then_exp[0];
-                               exponent[index] = cf_then_exp[1];
-                        }                     
+            //end create
+            
+            //split at space to seperate into indivudal terms
+            String [] idvdl_terms = txt_poly.split(" ");
+            
+            int index = 0;
+            for(String term: idvdl_terms) {
+            	int x_count = 0;
+                for(int j=0; j<term.length(); j++){
+                    if(term.charAt(j) == 'x'){
+                        x_count++;
                     }
-                    else{
-                                String [] cf_then_exp = no_minus[i].split("x");
-                               coefficient[index] = cf_then_exp[0];
-                               exponent[index] = cf_then_exp[1];
-                    }
-                    index++;
                 }
-            }
-
-            return Polynomial(coefficient, exponent);
-            //total terms = number of operators + 1 induct on number of terms, total terms is a postivie integer
-            //create an array with that length
-            //create an index incrememnter starting at 0
-            //split the array at the plus signs (removes all the plus signs)
-            //each of the substrings in the array have no plus signs then, so it is only possible that a negative sign exists
-            //split each the subarrays at minus sign into a new array
-            //then for all strings after the first thing in array, it is a minus poly
-            //for each string in the subarray, if it is the first string, then put it in the 
-            //current incrememneter index
-            //if x exists in the first string then get the exponent, otherwise it is the constant, so put 0 for the exponent slot
-            //otherwise, get the term after the x, put that in exponenent then put -*number in the coefficient
-            //increment the incrementer by 1
+                //constant term
+                if (x_count == 0){
+                	//check positivity
+                	if(term.charAt(0)=='-') {
+                		coefficient[index] = -1*Double.parseDouble(term);
+                		exponent[index] = 0;
+                	}
+                	else {
+                		coefficient[index] = Double.parseDouble(term);
+                		exponent[index] = 0;
+                	}
+                } 
+                //isn't constant
+                else{
+                   String [] cf_then_exp = term.split("x");
+                   //check if the first term of poly isn't a constant
+                   if(!term.contains("+")||!term.contains("-")) {
+                	   coefficient[index] = Double.parseDouble(cf_then_exp[0]);
+                	   exponent[index] = Integer.parseInt(cf_then_exp[1]);
+                   }
+                   //is a preceding term in polynomial
+                   else if(cf_then_exp[0].charAt(0)=='+') {
+                	   coefficient[index] = Double.parseDouble(cf_then_exp[0]);
+                	   exponent[index] = Integer.parseInt(cf_then_exp[1]);
+                   }
+                   else {
+                	   coefficient[index] = -1* Double.parseDouble(cf_then_exp[0]);
+                	   exponent[index] = Integer.parseInt(cf_then_exp[1]); 
+                   }
+                }                     
+                index++;
+            }          
+            this.Poly_Cf = coefficient;
+            this.Poly_Exp = exponent;
+            //Plan:
+            //replace each operator in the string with space before it
+            //split at the space, then we have an array with each of the individual terms 
+            //once have the individual terms
+            //do x count
+            //if have an x, split them at the x
+            //first thing in array is coeff, 2nd is exponent
+            //otherwise no x, so it is just a constant
+            //assign values correspondingly
     }
 
  	public Polynomial add(Polynomial argPoly)
-	{
-        //check how many exponents are shared between the polynomials
-        // a+b - intersected element = a or b
-        // create 2 new arrays with that length, that is the length of the polynomial we want to create
-        //copy all of this coeffs into the new coeffs array
-        // copy all exponents of this into the new exps array
-        //iterate through the other polynomials stuff
-        //if the exponent is equal to any of the elements in this exponents then add the corresponding coefficients together
-        //otherwise the expoenent isn't equal to any of the elements in this expoennts, so we want to add them to the new array
-        //iterate to find a 0 then add the exponent and the coeffient in the new list
-        //then sort both of the arrays accordingly
-        //then check the number of zeroes in the new arrays (coefficients -1+1 = 0)
-        //if there isn't any then create a polynomial with the arrays
-        //if there are then count them
-        // create a new array that is same length as the old one minues the number of zeroes
-        //iterate through the old list, whenever the coeffiient isn't a 0 then add it to the new list
+	{	
+ 		//get number of matching exponents
         int match = 0; 
         for(int i = 0; i<this.Poly_Cf.length;i++){
-            for(int j =0; j<argPoly.Poly_Cf.lenght; j++){
-                if(this.Poly_Cf[i]==argPoly.Poly_Cf[j]){
+            for(int j =0; j<argPoly.Poly_Cf.length; j++){
+                if(this.Poly_Exp[i]==argPoly.Poly_Exp[j]){
                     match++;
                 }
             }
         }
         
-        int total_length = this.Poly_Cf.length + argPoly.Poly_Cf.lenght - match;
+        //total length of union is sum of each - matching b/c double counted
+        int total_length = this.Poly_Cf.length + argPoly.Poly_Cf.length - match;
         double [] newCf = new double [total_length];
         int[] newExp = new int [total_length];
 
+        //copy over the contents of this into the first available array spots
         for(int i = 0; i< this.Poly_Cf.length; i++){
             newCf[i] = this.Poly_Cf[i];
             newExp[i] = this.Poly_Exp[i];
         }
 
+        //iterate through the argPoly stuff
         for(int j = 0; j< argPoly.Poly_Cf.length; j++){
-            boolean equals = false
-            for(int h = 0; h<newCf.length; h++){
-                if(newExp[h]==newExp[h]){
+        	//check if that exponent is shared
+            boolean equals = false;
+            //iterate through each of the spots filled in by this in new array
+            //if found set equals to true and then add the coefficient in that spot
+            for(int h = 0; h<this.Poly_Cf.length; h++){
+                if(newExp[h]==argPoly.Poly_Exp[j]){
                     equals = true; 
-                    newCf[h] += argPoly[j]; //add coeffient there
+                    newCf[h] += argPoly.Poly_Cf[j]; //add coefficient there
+                    break;
                 }
             }
-            // if went through all and couldn't find a matchign coefficinet
+            // if went through all and couldn't find a matching coefficient
+            // go to the first uninitialzied spot in new array then add the exponent and cf info
             if(equals == false){
-                int i = 0; 
-                while(newExp[i]!=0){
-                    i++; //find the last exponent of first
-                }
-                newExp[i] = argPoly.Poly_Exp[j];
-                newCf[i] = argPoly.Poly_Cf[j];
+            	for(int i = 1; i<newExp.length;i++) {
+            		if(newExp[i]==0) {
+                        newExp[i] = argPoly.Poly_Exp[j];
+                        newCf[i] = argPoly.Poly_Cf[j];
+                        break;
+            		}
+            	}
             }
         }
-
-
+        
+        //sort the array by exponents, bubble sort
         boolean swap = false;
 		int tempExp = 0;
 		double tempCoeff = 0;
@@ -139,7 +161,8 @@ public class Polynomial {
 					tempExp = newExp[j];
 					newExp[j] = newExp[j + 1];
 					newExp[j + 1] = tempExp;
-					tempCoeff = newCF[j];
+					//swap coeff to match
+					tempCoeff = newCf[j];
 					newCf[j] = newCf[j + 1];
 					newCf[j + 1] = tempCoeff;
 					swap = true;
@@ -149,32 +172,44 @@ public class Polynomial {
 				break;//no swaps needed break alr sorted
 			}
 		}
-
+		
+		//check if once added it became 0, -3+3 = 0
         int total_zeroes = 0; 
-        for(int i = 0; i<newExp;i++){
+        for(int i = 0; i < newCf.length;i++){
             if(newCf[i] == 0 ){
                 total_zeroes++;
             }
         }
 
         if(total_zeroes == 0){
-            return Polynomial(newCf, newExp);
+            return new Polynomial(newCf, newExp);
         }
-        else{
-            int no_zero_len = total_length - num_zeroes;
+        else{//something when added made it 0
+            int no_zero_len = total_length - total_zeroes;
             double [] nzp_coeff = new double[no_zero_len];
             int [] nzp_exp = new int[no_zero_len];
             int tracker = 0; 
-            for(int i = 0; i<newCF.length; i++){
-                if(newCF.length != 0){
+            for(int i = 0; i<newCf.length; i++){
+                if(newCf[i] != 0){
                     nzp_coeff[tracker] = newCf[i];
                     nzp_exp[tracker] = newExp[i];
                     tracker++;
                 }
             }
-            return Polynomial(nzp_coeff, nzp_exp);
+            return new Polynomial(nzp_coeff, nzp_exp);
         }
         }
+ 	
+ 	public double evaluate(double x) {
+        double result = 0.0;
+        int n = this.Poly_Cf.length;
+
+        for (int i = 0; i < n; i++) {
+            result += this.Poly_Cf[i] * Math.pow(x,this.Poly_Exp[i]);
+        }
+        return result;
+    }
+
 
     public boolean hasRoot(double x) {
         return this.evaluate(x) == 0;
@@ -187,28 +222,81 @@ public class Polynomial {
         //if the exponent is 0 then just update coefficient of new array
         //otherwise if the exponent is not 0 then update each index in exp array that it multiplies by by expsort[i]+ argexpsort[j]
         //also update each corresponding coefficient array by Coeffsort[i] * argCoeffsort[j];
-
-        int total = this.Poly_Cf.length + multiplier.Poly_Exp.length - 1; //check with induction
+    	
+    	//total number of terms of polynomial, verify with induction
+        int total = this.Poly_Cf.length + multiplier.Poly_Exp.length - 1;
+        //create arrays to store cf and exp
 		double[] mCoeff = new double[total];
 		int[] mExp = new int[total];
-		for (int i = 0; i < Coeffsort.length; i++) {
-			for (int j = 0; j < argCoeffsort.length; j++) {
-				if (mExp[i + j] != 0) {
+		
+		//for term in this poly, multiply it with all other terms
+		//for each term
+		//if the term 
+		for (int i = 0; i < this.Poly_Cf.length; i++) {
+			for (int j = 0; j < multiplier.Poly_Cf.length; j++) {
+				//set coeff equal to current this term multiplied by current j-multiplier term
+				//then iterate through all multiplier terms
+				mCoeff[i + j] += this.Poly_Cf[i] * multiplier.Poly_Cf[j];
+				//if the exponent isn't 0, then the corresponding exponents should be updated
+				if (this.Poly_Exp[i] != 0) {
 					mExp[i + j] = this.Poly_Exp[i] + multiplier.Poly_Exp[j];
-					mCoeff[i + j] += this.Poly_Cf[i] * multip   lier.Poly_Cf[j];
-				}
-				else {
-					resCoeff[i + j] += Coeffsort[i] * argCoeffsort[j];
-				}
+					
+				}					
 			}
 		}
+		
 		return new Polynomial(mCoeff, mExp);
 		
     }
 
-    public void saveToFile(String filename){
-
+    public void saveToFile(String poly_file) throws IOException{
+        BufferedWriter writer = new BufferedWriter(new FileWriter(poly_file));
+        
+        for(int i = 0; i<this.Poly_Cf.length; i++){
+        	//first term, can be a constant
+            if(i==0){
+            	//check positivity of the first coefficient
+            	//if positive just write the constant
+            	//then if the positive term isn't a constant then write x and the exponent
+                if(this.Poly_Cf[i]>0){
+                    writer.write(Double.toString(this.Poly_Cf[i]));
+                    if(this.Poly_Exp[i]!=0){
+                        writer.write('x'+Integer.toString(this.Poly_Exp[i]));
+                    }
+                }
+                // first term is negative, write - then the coefficient
+                // if isn't constant write x then the exponent
+                else{
+                    writer.write(Double.toString(this.Poly_Cf[i]));
+                    if(this.Poly_Exp[i]!=0){
+                        writer.write('x'+Integer.toString(this.Poly_Exp[i]));
+                    }
+                }  
+            	}
+            	//not the first term of the poly, must have an operator before it
+            	//also cannot be a constant
+            else{
+            	if(this.Poly_Cf[i]>0){
+            		writer.write("+" + Double.toString(this.Poly_Cf[i]));
+                    writer.write('x' + Integer.toString(this.Poly_Exp[i]));
+                }
+                //negative coeff
+            	else{
+            		writer.write(Double.toString(this.Poly_Cf[i]));
+                    writer.write('x'+Integer.toString(this.Poly_Exp[i]));
+                }
+            }      
+        }
+        writer.close();
+        //takes a polynomial and a file name then converts that polynomial to a stirng and saves it in the file
+        //iterate through the coefficient arrays
+        //for iteration
+        //if first iteration
+        //if the coefficient is positive, write the coefficient
+        //if the coefficient is negative, write - + cf
+        //check exponent
+        //if exponent is not 0 then write x and then the exponent
+        //otherwise do the same but with a '+' + cf if cf is positive
     }
 }
-
 
